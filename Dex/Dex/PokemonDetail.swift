06536c018c70f9e29e293bb954a3a1393496be97
dtharpeuno <dtharpe1@gmail.com>
2026-01-05 14:45:34 -0500

    store fetched images

--- Dex/Dex/PokemonDetail.swift
+++ Dex/Dex/PokemonDetail.swift
@@ -23,16 +23,21 @@ struct PokemonDetail: View {
 					.scaledToFit()
 					.shadow(color: .black, radius: 6)
 				
-				AsyncImage(url: showShiny ? pokemon.shinyURL : pokemon.spriteURL) { image in
-					image
-						.interpolation(.none)
-						.resizable()
-						.scaledToFit()
-						.padding(.top, 50)
-						.shadow(color: .black, radius: 6)
-				} placeholder: {
-					ProgressView()
+				if pokemon.sprite == nil || pokemon.shiny == nil {
+					AsyncImage(url: showShiny ? pokemon.shinyURL : pokemon.spriteURL) { image in
+						image
+							.interpolation(.none)
+							.resizable()
+							.scaledToFit()
+							.padding(.top, 50)
+							.shadow(color: .black, radius: 6)
+					} placeholder: {
+						ProgressView()
+					}
+				} else {
+					(showShiny ? pokemon.shinyImage : pokemon.spriteImage)
 				}
+	
 				
 			}
 			HStack {

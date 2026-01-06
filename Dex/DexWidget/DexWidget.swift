479f09dbe0564fa9cb6ed806ddb22143768caab0
dtharpeuno <dtharpe1@gmail.com>
2026-01-06 11:09:47 -0500

    3 pokemon widgets

--- Dex/DexWidget/DexWidget.swift
+++ Dex/DexWidget/DexWidget.swift
@@ -9,37 +9,37 @@ import WidgetKit
 import SwiftUI
 
 struct Provider: TimelineProvider {
-    func placeholder(in context: Context) -> SimpleEntry {
+	func placeholder(in context: Context) -> SimpleEntry {
 		SimpleEntry.placeholder
-    }
-
-    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry.placeholder
-        completion(entry)
-    }
-
-    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [SimpleEntry] = []
-
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
-        let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
+	}
+	
+	func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+		let entry = SimpleEntry.placeholder
+		completion(entry)
+	}
+	
+	func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+		var entries: [SimpleEntry] = []
+		
+		// Generate a timeline consisting of five entries an hour apart, starting from the current date.
+		let currentDate = Date()
+		for hourOffset in 0 ..< 5 {
 			_ = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry.placeholder
-            entries.append(entry)
-        }
-
-        let timeline = Timeline(entries: entries, policy: .atEnd)
-        completion(timeline)
-    }
-
-//    func relevances() async -> WidgetRelevances<Void> {
-//        // Generate a list containing the contexts this widget is relevant in.
-//    }
+			let entry = SimpleEntry.placeholder
+			entries.append(entry)
+		}
+		
+		let timeline = Timeline(entries: entries, policy: .atEnd)
+		completion(timeline)
+	}
+	
+	//    func relevances() async -> WidgetRelevances<Void> {
+	//        // Generate a list containing the contexts this widget is relevant in.
+	//    }
 }
 
 struct SimpleEntry: TimelineEntry {
-    let date: Date
+	let date: Date
 	let name: String
 	let types: [String]
 	let sprite: Image
@@ -54,37 +54,116 @@ struct SimpleEntry: TimelineEntry {
 }
 
 struct DexWidgetEntryView : View {
-    var entry: Provider.Entry
-
-    var body: some View {
-        VStack {
-			entry.sprite
-        }
-    }
+	@Environment(\.widgetFamily) var widgetSize
+	
+	var entry: Provider.Entry
+	
+	var pokemonImage: some View {
+		entry.sprite
+			.interpolation(.none)
+			.resizable()
+			.scaledToFit()
+			.shadow(color: .black, radius: 6)
+	}
+	
+	var typesView: some View {
+		ForEach(entry.types, id: \.self) {
+			type in
+			Text(type.capitalized)
+				.font(.subheadline)
+				.fontWeight(.semibold)
+				.foregroundStyle(.black)
+				.padding(.horizontal, 13)
+				.padding(.vertical, 5)
+				.background(Color(type.capitalized))
+				.clipShape(.capsule)
+		}
+	}
+	
+	var body: some View {
+		switch widgetSize {
+		case .systemMedium:
+			HStack {
+				pokemonImage
+				Spacer()
+				
+				VStack(alignment: .leading) {
+					Text(entry.name.capitalized)
+						.font(.title)
+						.padding(.vertical, -1)
+					
+					HStack {
+						typesView
+					}
+				}
+				.layoutPriority(1)
+				
+				Spacer()
+			}
+			
+		case .systemLarge:
+			ZStack {
+				pokemonImage
+				
+				VStack(alignment: .leading) {
+					Text(entry.name.capitalized)
+						.font(.largeTitle)
+						.lineLimit(1)
+					
+					Spacer()
+					
+					HStack {
+						Spacer()
+						typesView
+					}
+				}
+			}
+		default:
+			pokemonImage
+		}
+		
+	}
 }
 
 struct DexWidget: Widget {
-    let kind: String = "DexWidget"
-
-    var body: some WidgetConfiguration {
-        StaticConfiguration(kind: kind, provider: Provider()) { entry in
-            if #available(iOS 17.0, *) {
-                DexWidgetEntryView(entry: entry)
-                    .containerBackground(.fill.tertiary, for: .widget)
-            } else {
-                DexWidgetEntryView(entry: entry)
-                    .padding()
-                    .background()
-            }
-        }
-        .configurationDisplayName("My Widget")
-        .description("This is an example widget.")
-    }
+	let kind: String = "DexWidget"
+	
+	var body: some WidgetConfiguration {
+		StaticConfiguration(kind: kind, provider: Provider()) { entry in
+			if #available(iOS 17.0, *) {
+				DexWidgetEntryView(entry: entry)
+					.foregroundStyle(.black)
+					.containerBackground(Color(entry.types[0].capitalized), for: .widget)
+			} else {
+				DexWidgetEntryView(entry: entry)
+					.padding()
+					.background()
+			}
+		}
+		.configurationDisplayName("Pokemon")
+		.description("See a random Pokemon.")
+	}
 }
 
 #Preview(as: .systemSmall) {
-    DexWidget()
+	DexWidget()
 } timeline: {
 	SimpleEntry.placeholder
 	SimpleEntry.placeholderTwo
 }
+
+#Preview(as: .systemMedium) {
+	DexWidget()
+} timeline: {
+	SimpleEntry.placeholder
+	SimpleEntry.placeholderTwo
+}
+
+
+#Preview(as: .systemLarge) {
+	DexWidget()
+} timeline: {
+	SimpleEntry.placeholder
+	SimpleEntry.placeholderTwo
+}
+
